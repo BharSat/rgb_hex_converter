a9af27b4166b11b563184e45bf552982e6fcfa20
BharSat <69454005+BharSat@users.noreply.github.com>
2020-11-19 14:50:19 +0530

    Add files via upload

---
+++ rgb_hex_converter.py
@@ -0,0 +1,35 @@
+def hex_to_rgb(hexadecimal=('#', 'F', 'F', '0', '6', 'A', 'a')):
+    hexadecimal = list(hexadecimal)
+    hexadecimal = hexadecimal[1:]
+    count = 0
+
+    dict1 = {
+        'a': 10,
+        'b': 11,
+        'c': 12,
+        'd': 13,
+        'e': 14,
+        'f': 15
+    }
+
+    for i in hexadecimal:
+        hexadecimal[count] = dict1.get(i.lower(), i)
+        count += 1
+    r, r1, g, g1, b, b1 = list(hexadecimal)
+    r = (int(r)*16) + int(r1)
+    b = (int(b)*16) + int(b1)
+    g = (int(g)*16) + int(g1)
+    return r, g, b
+
+
+def rgb_to_hex(rgb=(200, 19, 255)):
+    final = []
+    for i in rgb:
+        py_hex = hex(i)
+        hex_ = py_hex.replace('0x', '')
+        final.append(hex_)
+    return '#' + final[0] + final[1] + final[2]
+
+
+if __name__ == '__main__':
+    print(rgb_to_hex(hex_to_rgb(hexadecimal=tuple('#FA23DE'))))
